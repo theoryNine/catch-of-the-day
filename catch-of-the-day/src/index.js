9dfdaa0c53b29fbfa1e8933118987eab7034dc7f
Ansel Colvin <anselcolvin@gmail.com>
2017-11-30 16:47:15 -0500

    Cleanup

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { render } from 'react-dom';
-import{ BrowserRouter, Match, Miss } from 'react-router';
-import './css/style.css';
-import App from './components/App';
-import NotFound from './components/NotFound';
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter, Match, Miss } from 'react-router'
+import './css/style.css'
+import App from './components/App'
+import NotFound from './components/NotFound'
 
-import StorePicker from './components/StorePicker';
+import StorePicker from './components/StorePicker'
 
 const Root = () => {
    return (
       <BrowserRouter>
          <div>
-            <Match exactly pattern="/" component={StorePicker} />
-            <Match pattern="/store/:storeId" component={App} />
+            <Match exactly pattern='/' component={StorePicker} />
+            <Match pattern='/store/:storeId' component={App} />
             <Miss component={NotFound} />
          </div>
       </BrowserRouter>
    )
 }
 
-render(<Root/>, document.querySelector('#main'));
+render(<Root/>, document.querySelector('#main'))
